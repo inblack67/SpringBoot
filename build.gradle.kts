14db8f09b013ce0d6879c2f86be7c7f4bd62076e
ryuzaki <ryuzaki>
2023-04-15 03:38:28 +0530

    open api

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,6 +20,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-validation")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.1.0")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
